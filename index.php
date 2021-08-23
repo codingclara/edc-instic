eebb1b036c89d23a2d84361bd4a0800a55a7b075
Clara Carrillo <likenaaa@gmail.com>
2021-08-23 14:36:17 +0200

    basic structure + en-tête

---
+++ index.php
@@ -0,0 +1,18 @@
+ <!DOCTYPE html>
+ <html>
+    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <link rel="stylesheet" href="style.css">
+        <title>home</title>
+    </head>
+    <body>
+    <div class="navbar">
+        <div class="home-link">
+            <a href="index.php">home</a>
+            <a href="ajout.php">sign-up</a>
+            <a href="recherche.php">research</a>
+        </div>
+    </div>
+    </body>
+ </html>
